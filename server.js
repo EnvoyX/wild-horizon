858deab2a4e4f1cd2b2cb3245173ad3022354780
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-06-12 20:43:22 +0700

    feat: added route not found

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 import http from "node:http";
+import { getDataFromDB } from "./database/db.js";
 
 const PORT = 8000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   if (req.url === "/api" && req.method === "GET") {
     console.log(req.url);
-    res.end("This is from the server");
+    const destination = await getDataFromDB();
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    res.write(JSON.stringify(destination));
+    res.end("Get all data");
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 404;
+    res.end(
+      JSON.stringify({
+        error: "not found",
+        message: "The requested route does not exist",
+      })
+    );
   }
 });
 
